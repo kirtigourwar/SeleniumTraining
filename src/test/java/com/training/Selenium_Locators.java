6a121d715e413a8a0f2b8883ec53a763ce7f5bfb
kgourwar <kgourwar@puneis0100320.medline.com>
2019-04-09 18:51:30 +0530

    Select Date changes

--- src/test/java/com/training/Selenium_Locators.java
+++ src/test/java/com/training/Selenium_Locators.java
@@ -2,19 +2,21 @@ package com.training;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.*;
 
 
 public class Selenium_Locators {
 	
 	public static WebDriver driver;
+	public String birthDate = "11/11/2002";
 	
 	@BeforeTest
 	public void setUp()
 	{   
 		
-		//System.setProperty("webdriver.chrome.driver","C:\D_Drive\Appium\Softwares\Driver\chromedriver_win32_v72\chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver","K:/LearnSelenium/Software/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:/D_Drive/Appium/Softwares/Driver/chromedriver_win32_v72/chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver","K:/LearnSelenium/Software/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 	}
@@ -31,10 +33,26 @@ public class Selenium_Locators {
 		driver.findElement(By.cssSelector("input#u_0_o"));
 		driver.findElement(By.xpath("//input[@name='reg_email__']")).sendKeys("testemail1@gmail.com");
 		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("facebookpwd@123");
-		
+		selectBirthDate(birthDate);
 		Thread.sleep(10);
 	
 		}
+	
+	@Parameters ({"Bdate"})
+	@Test
+	public void selectBirthDate (@Optional String date)
+	{
+		String DD[] = birthDate.split("/");
+		System.out.println(DD[0]);
+		
+		Select day = new Select(driver.findElement(By.id("day")));
+		Select month = new Select(driver.findElement(By.id("month")));
+		Select year = new Select(driver.findElement(By.id("year")));
+		day.selectByValue(DD[0]);
+		month.selectByValue(DD[1]);
+		year.selectByValue(DD[2]);
+	}
+	
 	
 	@Parameters ({"webDriver","webElement"})
 	@Test (priority =2)
@@ -43,9 +61,9 @@ public class Selenium_Locators {
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		js.executeScript("arguments[0].style.border = '2px solid red'", element);
 	}
-	@AfterTest
+	/*@AfterTest
 	public void Closure()
 	{
 	 driver.quit();	
-	}
+	}*/
 }
